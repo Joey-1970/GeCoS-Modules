5cec3229ed8884f5e432da263e25de8c2a142e7a
Joey-1970 <paeper@horburg.de>
2017-02-16 15:46:59 +0100

    Update module.php

--- GeCoS_PWM16Out/module.php
+++ GeCoS_PWM16Out/module.php
@@ -9,7 +9,7 @@
             	parent::Create();
  	    	$this->RegisterPropertyBoolean("Open", false);
 		$this->ConnectParent("{5F50D0FC-0DBB-4364-B0A3-C900040C5C35}");
- 	    	$this->RegisterPropertyInteger("DeviceAddress", 32);
+ 	    	$this->RegisterPropertyInteger("DeviceAddress", 64);
 		$this->RegisterPropertyInteger("DeviceBus", 1);
         }
  	
@@ -24,7 +24,8 @@
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
  		
 		$arrayOptions = array();
-		$arrayOptions[] = array("label" => "32 dez. / 0x20h", "value" => 32);
+		$arrayOptions[] = array("label" => "64 dez. / 0x40h", "value" => 64);
+		$arrayOptions[] = array("label" => "65 dez. / 0x41h", "value" => 65);
 		$arrayElements[] = array("type" => "Select", "name" => "DeviceAddress", "caption" => "Device Adresse", "options" => $arrayOptions );
 		
 		$arrayElements[] = array("type" => "Label", "label" => "GeCoS I²C-Bus");
