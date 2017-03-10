6eeb7f256ad71ff1585c199a961853694f1f61b4
Joey-1970 <paeper@horburg.de>
2017-03-10 22:42:01 +0100

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -71,7 +71,7 @@
 		
 			
 			If ($this->ReadPropertyBoolean("Open") == true) {
-				//$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 				
 				// Setup
 				//$this->Setup();
