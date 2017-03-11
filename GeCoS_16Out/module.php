3a8ab5b432b1f5cec591035d2666d54c73452b51
Joey-1970 <paeper@horburg.de>
2017-03-11 20:14:04 +0100

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -131,7 +131,7 @@
 	private function Setup()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
-		
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Command" => XXXX)));
 		}
 	}
 }
