1eb8261b65d58851c82d79681fa8b4b8e219c171
Joey-1970 <paeper@horburg.de>
2017-03-11 21:26:26 +0100

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -218,7 +218,8 @@ class GeCoS_IO extends IPSModule
 			break;  
 		case "i2c_write_bytes":
 		   	//IPS_LogMessage("IPS2GPIO I2C Write Byte Handle: ","DeviceAdresse: ".$data->DeviceAddress.", Handle: ".$this->GetI2C_DeviceHandle($data->DeviceAddress).", Wert: ".$data->Value);  	
-		   	If ($this->GetI2C_DeviceHandle($data->DeviceIdent) >= 0) {
+		   	IPS_LogMessage("GeCoS_IO","DeviceIdent: ".$data->DeviceIdent); 
+			If ($this->GetI2C_DeviceHandle($data->DeviceIdent) >= 0) {
 		   		$this->SetMUX($data->DeviceIdent >> 7);
 				IPS_LogMessage("GeCoS_IO","DeviceAdresse: ".$data->DeviceAddress.", Handle: ".$this->GetI2C_DeviceHandle($data->DeviceAddress).", Wert: ".$data->Value.", Bus: ".$data->DeviceIdent >> 7); 
 				$this->CommandClientSocket(pack("L*", 57, intval($this->GetI2C_DeviceHandle($data->DeviceIdent)), 0, strlen($data->Command)).$data->Command, 16);
