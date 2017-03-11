172966bdcd586ce34b327e52177683258d1a75ef
Joey-1970 <paeper@horburg.de>
2017-03-11 20:28:17 +0100

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -74,9 +74,9 @@
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
 				
 				// Setup
-				//$this->Setup();
+				$this->Setup();
 				// Erste Messdaten einlesen
-				//$this->Measurement();
+				$this->Measurement();
 				$this->SetStatus(102);
 			}
 			else {
@@ -124,7 +124,8 @@
 	private function Measurement()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
-		
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Command" => hexdec("02FF")));
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Command" => hexdec("03FF")));
 		}
 	}	
 	
