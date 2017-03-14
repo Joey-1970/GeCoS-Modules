d8b3b37aebadfc21507b648696f76f21938269cd
Joey-1970 <paeper@horburg.de>
2017-03-14 21:20:06 +0100

    Update module.php

--- GeCoS_16In/module.php
+++ GeCoS_16In/module.php
@@ -55,17 +55,17 @@
 		
 		//Status-Variablen anlegen
 		for ($i = 0; $i <= 15; $i++) {
-			$this->RegisterVariableBoolean("Output_X".$i, "Ausgang X".$i, "~Switch", ($i + 1) * 10);
+			$this->RegisterVariableBoolean("Input_X".$i, "Eingang X".$i, "", ($i + 1) * 10);
 			$this->EnableAction("Output_X".$i);	
 		}
 		
-		$this->RegisterVariableInteger("OutputBank0", "Output Bank 0", "", 170);
-          	$this->DisableAction("OutputBank0");
-		IPS_SetHidden($this->GetIDForIdent("OutputBank0"), false);
+		$this->RegisterVariableInteger("InputBank0", "Input Bank 0", "", 170);
+          	$this->DisableAction("InputBank0");
+		IPS_SetHidden($this->GetIDForIdent("InputBank0"), false);
 		
-		$this->RegisterVariableInteger("OutputBank1", "Output Bank 1", "", 180);
-          	$this->DisableAction("OutputBank1");
-		IPS_SetHidden($this->GetIDForIdent("OutputBank1"), false);
+		$this->RegisterVariableInteger("InputBank1", "Input Bank 1", "", 180);
+          	$this->DisableAction("InputBank1");
+		IPS_SetHidden($this->GetIDForIdent("InputBank1"), false);
 		
 		If (IPS_GetKernelRunlevel() == 10103) {
 			// Logging setzen
@@ -81,7 +81,7 @@
 				
 				// Setup
 				$this->Setup();
-				$this->GetOutput();
+				$this->GetInput();
 				$this->SetStatus(102);
 			}
 			else {
@@ -116,20 +116,20 @@
 			   	If ($data->DeviceIdent == $this->GetBuffer("DeviceIdent")) {
 			   		$ByteArray = array();
 					$ByteArray = unserialize($data->ByteArray);
-					SetValueInteger($this->GetIDForIdent("OutputBank0"), $ByteArray[1]);
-					SetValueInteger($this->GetIDForIdent("OutputBank1"), $ByteArray[2]);
+					SetValueInteger($this->GetIDForIdent("InputBank0"), $ByteArray[1]);
+					SetValueInteger($this->GetIDForIdent("InputBank1"), $ByteArray[2]);
 					//IPS_LogMessage("GeCoS_16Out", "Bank 0: ".$ByteArray[1]);
 					//IPS_LogMessage("GeCoS_16Out", "Bank 1: ".$ByteArray[2]);
 					for ($i = 0; $i <= 7; $i++) {
 						$Bitvalue = boolval($ByteArray[1]&(1<<$i));					
-					    	If (GetValueBoolean($this->GetIDForIdent("Output_X".$i)) <> $Bitvalue) {
-							SetValueBoolean($this->GetIDForIdent("Output_X".$i), $Bitvalue);
+					    	If (GetValueBoolean($this->GetIDForIdent("Input_X".$i)) <> $Bitvalue) {
+							SetValueBoolean($this->GetIDForIdent("Input_X".$i), $Bitvalue);
 						}
 					}
 					for ($i = 8; $i <= 15; $i++) {
 						$Bitvalue = boolval($ByteArray[2]&(1<<($i - 8)));					
-					    	If (GetValueBoolean($this->GetIDForIdent("Output_X".$i)) <> $Bitvalue) {
-							SetValueBoolean($this->GetIDForIdent("Output_X".$i), $Bitvalue);
+					    	If (GetValueBoolean($this->GetIDForIdent("Input_X".$i)) <> $Bitvalue) {
+							SetValueBoolean($this->GetIDForIdent("Input_X".$i), $Bitvalue);
 						}
 					}
 			   	}
@@ -141,120 +141,20 @@
 	{
   		//SetValueBoolean($this->GetIDForIdent($Ident), $Value);
 		switch($Ident) {
-		case "Output_X0":
-	            $this->SetOutputPin(0, $Value);
-	            break;
-	        case "Output_X1":
-	            $this->SetOutputPin(1, $Value);
-	            break;
-	        case "Output_X2":
-	            $this->SetOutputPin(2, $Value);
-	            break;
-	        case "Output_X3":
-	            $this->SetOutputPin(3, $Value);
-	            break;
-	        case "Output_X4":
-	            $this->SetOutputPin(4, $Value);
-	            break;
-	        case "Output_X5":
-	            $this->SetOutputPin(5, $Value);
-	            break;    
-	        case "Output_X6":
-	            $this->SetOutputPin(6, $Value);
-	            break;
-	        case "Output_X7":
-	            $this->SetOutputPin(7, $Value);
-	            break;
-	        case "Output_X8":
-	            $this->SetOutputPin(8, $Value);
-	            break;
-	        case "Output_X9":
-	            $this->SetOutputPin(9, $Value);
-	            break;
-	        case "Output_X10":
-	            $this->SetOutputPin(10, $Value);
-	            break;
-	        case "Output_X11":
-	            $this->SetOutputPin(11, $Value);
-	            break;
-	        case "Output_X12":
-	            $this->SetOutputPin(12, $Value);
-	            break;
-	        case "Output_X13":
-	            $this->SetOutputPin(13, $Value);
-	            break;    
-	        case "Output_X14":
-	            $this->SetOutputPin(14, $Value);
-	            break;
-	        case "Output_X15":
-	            $this->SetOutputPin(15, $Value);
-	            break;
+		
 	        default:
 	            throw new Exception("Invalid Ident");
 	    	}
 	}
 	    
 	// Beginn der Funktionen
-	public function SetOutputPin(Int $Output, Bool $Value)
-	{
-		$Output = min(15, max(0, $Output));
-		$Value = min(1, max(0, $Value));
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			If ($Output <= 7) {
-				$Bitmask = GetValueInteger($this->GetIDForIdent("OutputBank0"));
-				If ($Value == true) {
-					$Bitmask = $this->setBit($Bitmask, $Output);
-				}
-				else {
-					$Bitmask = $this->unsetBit($Bitmask, $Output);
-				}
-				$ByteArray = array();
-				$ByteArray[0] = hexdec("02");
-				$ByteArray[1] = $Bitmask;
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
-			}
-			else {
-				$Bitmask = GetValueInteger($this->GetIDForIdent("OutputBank1"));
-				If ($Value == true) {
-					$Bitmask = $this->setBit($Bitmask, $Output - 8);
-				}
-				else {
-					$Bitmask = $this->unsetBit($Bitmask, $Output - 8);
-				}
-				$ByteArray = array();
-				$ByteArray[0] = hexdec("03");
-				$ByteArray[1] = $Bitmask;
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
-			}
-			$this->GetOutput();
-		}
-	}	
-	
-	private function GetOutput()
+	private function GetInput()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 2)));
 		}
 	}
-	    
-	public function SetOutputBank(int $Bank, int $Value) 
-	{
-		$Value = min(255, max(0, $Value));
-		$Bank = min(1, max(0, $Bank));
-		$ByteArray = array();
-		If ($Bank == 0) {
-			$ByteArray[0] = hexdec("02");
-			$ByteArray[1] = $Value;
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
-		}
-		else {
-			$ByteArray[0] = hexdec("03");
-			$ByteArray[1] = $Value;
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
-		}
-		$this->GetOutput();
-	}
-	    
+	        
 	private function Setup()
 	{
 		If ($this->ReadPropertyBoolean("Open") == true) {
@@ -267,16 +167,6 @@
 			$ByteArray[1] = hexdec("00");
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
 		}
-	}
-	
-	private function setBit($byte, $significance) { 
- 		// ein bestimmtes Bit auf 1 setzen
- 		return $byte | 1<<$significance;   
- 	} 
-	
-	private function unsetBit($byte, $significance) {
-	    // ein bestimmtes Bit auf 0 setzen
-	    return $byte & ~(1<<$significance);
 	}
 }
 ?>
