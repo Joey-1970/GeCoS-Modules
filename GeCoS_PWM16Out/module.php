bfd84feba442813f584e4c2c006875b2531ec754
Joey-1970 <paeper@horburg.de>
2017-03-16 19:45:41 +0100

    Update module.php

--- GeCoS_PWM16Out/module.php
+++ GeCoS_PWM16Out/module.php
@@ -161,8 +161,45 @@
 	// Beginn der Funktionen
 	public function SetOutputPin(Int $Output, Bool $Value)
 	{ 
-		
+		$Output = min(15, max(0, $Output));
+		$Value = min(255, max(0, $Value));
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			If ($Output <= 7) {
+				$Bitmask = GetValueInteger($this->GetIDForIdent("OutputBank0"));
+				If ($Value == true) {
+					$Bitmask = $this->setBit($Bitmask, $Output);
+				}
+				else {
+					$Bitmask = $this->unsetBit($Bitmask, $Output);
+				}
+				$ByteArray = array();
+				$ByteArray[0] = hexdec("02");
+				$ByteArray[1] = $Bitmask;
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
+			}
+			else {
+				$Bitmask = GetValueInteger($this->GetIDForIdent("OutputBank1"));
+				If ($Value == true) {
+					$Bitmask = $this->setBit($Bitmask, $Output - 8);
+				}
+				else {
+					$Bitmask = $this->unsetBit($Bitmask, $Output - 8);
+				}
+				$ByteArray = array();
+				$ByteArray[0] = hexdec("03");
+				$ByteArray[1] = $Bitmask;
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "ByteArray" => serialize($ByteArray) )));
+			}
+			$this->GetOutput();
+		}
 	}
+	
+	private function GetOutput()
+	{
+		If ($this->ReadPropertyBoolean("Open") == true) {
+			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_bytes", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 2)));
+		}
+	}    
 	    
 	private function Setup()
 	{
