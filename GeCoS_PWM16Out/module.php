38c527221890a46ae3813cd14abef6bff73d87c5
Joey-1970 <paeper@horburg.de>
2017-03-27 20:58:23 +0200

    Update module.php

--- GeCoS_PWM16Out/module.php
+++ GeCoS_PWM16Out/module.php
@@ -59,7 +59,7 @@
 		for ($i = 0; $i <= 15; $i++) {
 			$this->RegisterVariableBoolean("Output_Bln_X".$i, "Ausgang X".$i, "~Switch", ($i + 1) * 10);
 			$this->EnableAction("Output_Bln_X".$i);	
-			$this->RegisterVariableFloat("Output_Flt_X".$i, "Ausgang X".$i, "Intensity.4096", (($i + 1) * 10) + 5);
+			$this->RegisterVariableInteger("Output_Flt_X".$i, "Ausgang X".$i, "Intensity.4096", (($i + 1) * 10) + 5);
 			$this->EnableAction("Output_Flt_X".$i);	
 		}
 		
