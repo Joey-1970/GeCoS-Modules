5c7f74450f5b630531e828b0cc7ece0754e2f7fd
Joey-1970 <paeper@horburg.de>
2017-03-27 20:21:33 +0200

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -25,7 +25,7 @@
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
  		
 		$arrayOptions = array();
-		for ($i = 24; $i <= 31; $i = $i++) {
+		for ($i = 24; $i <= 31; $i++) {
 		    	$arrayOptions[] = array("label" => $i." dez. / 0x".strtoupper(dechex($i))."h", "value" => $i);
 		}
 		$arrayElements[] = array("type" => "Select", "name" => "DeviceAddress", "caption" => "Device Adresse", "options" => $arrayOptions );
