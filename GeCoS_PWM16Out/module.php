f7ef9c6a701353e51c29528593834de10453c1f8
Joey-1970 <paeper@horburg.de>
2017-03-31 17:11:40 +0200

    Update module.php

--- GeCoS_PWM16Out/module.php
+++ GeCoS_PWM16Out/module.php
@@ -77,7 +77,6 @@
 				
 				// Setup
 				$this->Setup();
-				$this->GetOutput();
 				$this->SetStatus(102);
 			}
 			else {
@@ -204,17 +203,7 @@
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $StartAddress + 2)));
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $StartAddress + 3)));
 		}
-	}    
-	    
-	private function GetOutput()
-	{
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			for ($i = 6; $i < 70; $i = $i + 4) {
-	    			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i + 2)));
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i + 3)));
-			}
-		}
-	}    
+	}     
 	    
 	private function Setup()
 	{
@@ -228,6 +217,11 @@
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => 0, "Value" => 0)));
 			// Mode 2 auf Ausgänge setzen
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_write_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => 1, "Value" => 4)));
+			// Ausgänge initial einlesen
+			for ($i = 6; $i < 70; $i = $i + 4) {
+	    			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i + 2)));
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_byte", "DeviceIdent" => $this->GetBuffer("DeviceIdent"), "Register" => $i + 3)));
+			}
 		}
 	}
 	
