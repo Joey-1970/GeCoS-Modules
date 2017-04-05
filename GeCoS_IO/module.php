92fe259cd6374d6becfce97ba653a4ac3ab11ff0
Joey-1970 <paeper@horburg.de>
2017-04-05 20:10:11 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -489,6 +489,7 @@ class GeCoS_IO extends IPSModule
 	private function CommandClientSocket(String $message, $ResponseLen = 16)
 	{
 		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->GetParentStatus() == 102)) {
+			$Result = -999;
 			if (IPS_SemaphoreEnter("CommandClientSocket", 25))
 			{
 				// Socket erstellen
