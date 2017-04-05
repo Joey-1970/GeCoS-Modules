cfef8ba553014387af5bbe5dfe0e5af4b4917953
Joey-1970 <paeper@horburg.de>
2017-04-05 15:51:22 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -673,7 +673,16 @@ class GeCoS_IO extends IPSModule
            				IPS_LogMessage("GeCoS_IO I2C Write Bytes","Handle: ".$response[2]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));
            			}
 		            	break;
-		        case "61":
+		        case "59":
+		            	If ($response[4] >= 0) {
+		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
+		            		//$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_data", "DeviceIdent" => $this->GetI2C_HandleDevice($response[2]), "Register" => $response[3], "Value" => $response[4])));
+		            	}
+		            	else {
+		            		IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));	
+		            	}
+		            	break;
+			case "61":
 		            	If ($response[4] >= 0) {
 		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
 		            		$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_data", "DeviceIdent" => $this->GetI2C_HandleDevice($response[2]), "Register" => $response[3], "Value" => $response[4])));
@@ -1018,7 +1027,7 @@ class GeCoS_IO extends IPSModule
 					$Handle = $this->GetI2C_DeviceHandle($DeviceIdent);
 					IPS_LogMessage("GeCoS_IO I2C-Suche","DeviceAddresse: ".$i." an Bus: ".($j - 4)." hat Handle: ".$Handle);
 					// Testweise lesen
-					$Result = $this->CommandClientSocket(pack("L*", 61, $SearchArray[$i], 0, 0), 16);
+					$Result = $this->CommandClientSocket(pack("L*", 59, $SearchArray[$i], 0, 0), 16);
 					IPS_LogMessage("GeCoS_IO I2C-Suche","Result ".$Result);
 					// Handle löschen
 					$this->CommandClientSocket(pack("LLLL", 55, $Handle, 0, 0), 16);
