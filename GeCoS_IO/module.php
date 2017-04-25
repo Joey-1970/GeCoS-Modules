2ec1bc6808abaaec83d31545659e599492bec31c
Joey-1970 <paeper@horburg.de>
2017-04-25 19:49:41 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -1070,7 +1070,7 @@ class GeCoS_IO extends IPSModule
 			$this->SetBuffer("InstanceArray", serialize($InstanceArray));
 			SetValueString($this->GetIDForIdent("Test"), serialize($InstanceArray));
 			
-			for ($i = $MinHandle; $i < max($maxHandle, 32) ; $i++) {
+			for ($i = $MinHandle; $i < max($MaxHandle, 32) ; $i++) {
 				$this->CommandClientSocket(pack("L*", 55, $i, 0, 0), 16);
 			}
 		}
