031206a54e8fb5a6d2db684e4974bd8fd10a139e
Joey-1970 <paeper@horburg.de>
2017-04-25 22:03:55 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -954,9 +954,9 @@ class GeCoS_IO extends IPSModule
 				return;
 			}
 			
-			//$FileContent = $sftp->file_get_contents($Path);
+			$FileContent = $sftp->get($Path);
 			
-			//IPS_LogMessage("GeCoS_IO Config.txt", $FileContent);
+			IPS_LogMessage("GeCoS_IO Config.txt", $FileContent);
 		}
 			
 	return;
