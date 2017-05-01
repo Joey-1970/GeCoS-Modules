54f0dfd7664ff8cedead79f43b3ff9ef52fcedbe
Joey-1970 <paeper@horburg.de>
2017-05-01 15:28:51 +0200

    Update module.php

--- GeCoS_RGBW/module.php
+++ GeCoS_RGBW/module.php
@@ -77,12 +77,12 @@
 		}
 		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
-			//ReceiveData-Filter setzen
-			$Filter = '((.*"Function":"get_used_i2c".*|.*"InstanceID":'.$this->InstanceID.'.*)|.*"Function":"status".*)';
-			$this->SetReceiveDataFilter($Filter);
-			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
-
 			If ($this->ReadPropertyBoolean("Open") == true) {
+				//ReceiveData-Filter setzen
+				$Filter = '((.*"Function":"get_used_i2c".*|.*"InstanceID":'.$this->InstanceID.'.*)|.*"Function":"status".*)';
+				$this->SetReceiveDataFilter($Filter);
+				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));
+
 				// Setup
 				$this->Setup();
 			}
