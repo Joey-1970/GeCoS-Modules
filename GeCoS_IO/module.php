c9a677fd6a9052f4830333dfaf801ab028e693d5
Joey-1970 <paeper@horburg.de>
2017-05-08 16:37:29 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -714,6 +714,15 @@ class GeCoS_IO extends IPSModule
 					//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));	
 		            	}
 		            	break;
+			case "60":
+		            	If ($response[4] >= 0) {
+		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
+					//$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_data", "InstanceID" => $this->InstanceArraySearch("Handle", $response[2]), "Register" => $response[3], "Value" => $response[4])));
+		            	}
+		            	else {
+		            		IPS_LogMessage("GeCoS_IO I2C Read Byte on Handle","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));	
+		            	}
+		            	break;
 			case "61":
 		            	If ($response[4] >= 0) {
 		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
@@ -1544,7 +1553,7 @@ class GeCoS_IO extends IPSModule
     		$this->SendDebug("OWReset", "I2C Reset", 0);
     		$this->SetMUX(1);
 		
-		$Result = $this->CommandClientSocket(pack("L*", 62, $this->GetBuffer("OW_Handle"), 0, 4, 180), 16);//1-wire reset
+		$Result = $this->CommandClientSocket(pack("L*", 60, $this->GetBuffer("OW_Handle"), 180, 0), 16);//1-wire reset
 		
 		If ($Result < 0) {
 			$this->SendDebug("OWReset", "I2C Reset Failed", 0);
