0f3520a539140f2fb0328de012936283c2d6788e
Joey-1970 <paeper@horburg.de>
2017-05-16 17:18:06 +0200

    Update module.php

--- GeCoS_DS1820/module.php
+++ GeCoS_DS1820/module.php
@@ -86,6 +86,7 @@
 				$this->SetReceiveDataFilter($Filter);
 				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_OWDevices", "DeviceSerial" => $this->ReadPropertyString("DeviceSerial"), "InstanceID" => $this->InstanceID)));		
 				$this->SetTimerInterval("Messzyklus", ($this->ReadPropertyInteger("Messzyklus") * 1000));
+				$this->Setup();
 				$this->Measurement();
 				$this->SetStatus(102);
 			}
