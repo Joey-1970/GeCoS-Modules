d3ad3445542090755ae0b97b62ed50be66e7c132
Joey-1970 <paeper@horburg.de>
2017-06-16 22:54:08 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -2282,7 +2282,7 @@ class GeCoS_IO extends IPSModule
 			$raw = ($raw ^ 0xffff) + 1;
 		} // negative, 2's compliment
 		
-		//$raw = $raw >> 3; 
+		$raw = $raw >> 3; 
 		
 		$Celsius = $raw / 16.0;
 		if ($SignBit) {
@@ -2293,7 +2293,7 @@ class GeCoS_IO extends IPSModule
 		$raw = ($data[4] << 8) | $data[3];
 		$raw = $raw & 0x3FF;
 		
-		$Voltage = $raw * 0.01 / 2;
+		$Voltage = $raw * 0.01;
 		
 		// Strom ermitteln
 		$raw = ($data[6] << 8) | $data[5];
