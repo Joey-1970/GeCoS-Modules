2c51b31bb556f2e38097200c073478538833b938
Joey-1970 <paeper@horburg.de>
2017-06-27 18:54:08 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -1306,8 +1306,8 @@ class GeCoS_IO extends IPSModule
 	private function SSH_Connect(String $Command)
 	{
 	        If (($this->ReadPropertyBoolean("Open") == true) ) {
-			set_include_path(__DIR__);
-			require_once (__DIR__ . '/libs/Net/SSH2.php');
+			set_include_path(__DIR__."/libs");
+			require_once (__DIR__ . '/Net/SSH2.php');
 			$ssh = new Net_SSH2($this->ReadPropertyString("IPAddress"));
 			$login = @$ssh->login($this->ReadPropertyString("User"), $this->ReadPropertyString("Password"));
 			if ($login == false)
@@ -1330,8 +1330,8 @@ class GeCoS_IO extends IPSModule
 	private function SSH_Connect_Array(String $Command)
 	{
 	        If (($this->ReadPropertyBoolean("Open") == true) AND ($this->GetParentStatus() == 102)) {
-			set_include_path(__DIR__);
-			require_once (__DIR__ . '/libs/Net/SSH2.php');
+			set_include_path(__DIR__."/libs");
+			require_once (__DIR__ . '/Net/SSH2.php');
 			$ssh = new Net_SSH2($this->ReadPropertyString("IPAddress"));
 			$login = @$ssh->login($this->ReadPropertyString("User"), $this->ReadPropertyString("Password"));
 			if ($login == false)
@@ -1369,11 +1369,9 @@ class GeCoS_IO extends IPSModule
 		$arrayCheckConfig["PIGPIO Server"]["Status"] = "unbekannt";
 		$arrayCheckConfig["PIGPIO Server"]["Color"] = "#FFFF00";
 		
-		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->GetParentStatus() == 102)) {
-			
-			
-			set_include_path(__DIR__);
-			require_once (__DIR__ . '/libs/Net/SFTP.php');
+		If (($this->ReadPropertyBoolean("Open") == true) AND ($this->GetParentStatus() == 102)) {			
+			set_include_path(__DIR__."/libs");
+			require_once (__DIR__ . '/Net/SFTP.php');
 			$sftp = new Net_SFTP($this->ReadPropertyString("IPAddress"));
 			$login = @$sftp->login($this->ReadPropertyString("User"), $this->ReadPropertyString("Password"));
 			
