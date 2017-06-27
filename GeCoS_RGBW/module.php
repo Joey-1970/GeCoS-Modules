ddd0499aaeb74d4c02b85057252cbd3fac53b319
Joey-1970 <paeper@horburg.de>
2017-06-27 10:38:13 +0200

    Update module.php

--- GeCoS_RGBW/module.php
+++ GeCoS_RGBW/module.php
@@ -75,7 +75,7 @@
 			$this->EnableAction("Intensity_W_".($i + 1));			
 		}
 		
-		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {
+		If ((IPS_GetKernelRunlevel() == 10001) AND ($this->HasActiveParent() == true)) {
 			If ($this->ReadPropertyBoolean("Open") == true) {
 				//ReceiveData-Filter setzen
 				$Filter = '((.*"Function":"get_used_i2c".*|.*"InstanceID":'.$this->InstanceID.'.*)|.*"Function":"status".*)';
