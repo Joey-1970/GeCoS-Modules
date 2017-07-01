1aac802f18fa40f6ba3e0f752c2cc75088d370cc
Joey-1970 <paeper@horburg.de>
2017-07-01 09:16:18 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -1013,7 +1013,7 @@ class GeCoS_IO extends IPSModule
 				
 				stream_set_timeout($this->Socket, 5);
 				stream_socket_sendto($this->Socket, $Data);
-				$buf = fgets(!$this->Socket, $ResponseLen + 1);
+				$buf = fgets($this->Socket, $ResponseLen + 1);
 				// Anfragen mit variabler Rückgabelänge
 				$CmdVarLen = array(56, 67, 70, 73, 75, 80, 88, 91, 92, 106, 109);
 				$MessageArray = unpack("L*", $buf);
