41b2cd4dc0462be3b872b977b53769a321b26af3
Joey-1970 <paeper@horburg.de>
2017-07-02 20:22:02 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -950,7 +950,8 @@ class GeCoS_IO extends IPSModule
 						$this->SendDebug("CommandClientSocket", "Fehler beim Verbindungsaufbau ".$errno." ".$errstr, 0);
 						If ($errno == 10060) {
 							$this->SetStatus(201);
-							// Starte Timer
+							// Testballon über IPS-ClientSocket senden
+							$this->ClientSocket(pack("LLLL", 17, 0, 0, 0).pack("LLLL", 26, 0, 0, 0));
 						}
 						IPS_SemaphoreLeave("CommandClientSocket");
 						return $Result;
