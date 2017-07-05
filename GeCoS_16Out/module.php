757f3eed303c1cf186f560f5934e5eaf3e5a18ef
Joey-1970 <paeper@horburg.de>
2017-07-05 23:25:00 +0200

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -152,7 +152,7 @@
 			//$Result= $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_PCA9655E_Read", "InstanceID" => $this->InstanceID, "Register" => 2, "Count" => 2)));
 			IPS_Sleep(100);
 			$Result= $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_PCA9655E_Read", "InstanceID" => $this->InstanceID, "Register" => 2)));
-			if (($Result === NULL) OR ($Result > 0) OR ($Result > pow(2,16))) {// Falls der Splitter einen Fehler hat und 'nichts' zurückgibt.
+			if (($Result === NULL) OR ($Result < 0) OR ($Result > pow(2,16))) {// Falls der Splitter einen Fehler hat und 'nichts' zurückgibt.
 				$this->SendDebug("GetOutput", "Keine gültige Antwort!", 0);
 				return;
 			}
