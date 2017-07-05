a4b8c7faf9871573eb9b34fc290f4018d5c174b8
Joey-1970 <paeper@horburg.de>
2017-07-05 12:19:17 +0200

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -168,6 +168,7 @@
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_read_bytes", "InstanceID" => $this->InstanceID, "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 2)));
 			/*
+			
 			$Result= $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_PCA9655E_Read", "InstanceID" => $this->InstanceID, "Register" => $this->ReadPropertyInteger("DeviceAddress"), "Count" => 2)));
 			if ($Result === NULL) {// Falls der Splitter einen Fehler hat und 'nichts' zurückgibt.
 				$this->SendDebug("GetOutput", "Keine gültige Antwort!", 0);
@@ -177,6 +178,17 @@
 			$ByteArray = unserialize($Result); 
 			$ByteArray[3] = ($ByteArray[2] << 8) | $ByteArray[1];
 			$this->SendDebug("GetOutput", "Bank 0: ".$ByteArray[1]." Bank 1: ".$ByteArray[2]." Summe: ".$ByteArray[3], 0);
+			$this->SetBuffer("OutputBank0", $ByteArray[1]);
+			$this->SetBuffer("OutputBank1", $ByteArray[2]);
+
+			$ByteArray[3] = ($ByteArray[2] << 8) | $ByteArray[1];
+			$this->SendDebug("set_i2c_byte_block", "Bank 0: ".$ByteArray[1]." Bank 1: ".$ByteArray[2]." Summe: ".$ByteArray[3], 0);
+			for ($i = 0; $i <= 15; $i++) {
+				$Bitvalue = boolval($ByteArray[3] & pow(2, $i));					
+				If (GetValueBoolean($this->GetIDForIdent("Output_X".$i)) <> $Bitvalue) {
+					SetValueBoolean($this->GetIDForIdent("Output_X".$i), $Bitvalue);
+				}
+			}
 			*/
 		}
 	}
