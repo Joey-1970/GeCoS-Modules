ffcb61a03777174d471e0414a224ae4bba5cf1b0
Joey-1970 <paeper@horburg.de>
2017-07-11 21:35:22 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -841,6 +841,7 @@ class GeCoS_IO extends IPSModule
 						$Bitvalue_17 = boolval($Level & pow(2, 17));
 						If ($Bit17Read == false) {
 							$this->SendDebug("Datenanalyse", "Event: Interrupt - Bit 17 (I2C-Bus 0): ".(int)$Bitvalue_17, 0);
+							IPS_Sleep(100);
 							$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"interrupt", "DeviceBus" => 4)));
 							$Bit17Read = true;
 						}
@@ -849,6 +850,7 @@ class GeCoS_IO extends IPSModule
 						$Bitvalue_27 = boolval($Level & pow(2, 27));
 						If ($Bit27Read == false) {
 							$this->SendDebug("Datenanalyse", "Event: Interrupt - Bit 27 (I2C-Bus 1): ".(int)$Bitvalue_27, 0);
+							IPS_Sleep(100);
 							$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"interrupt", "DeviceBus" => 5)));
 							$Bit27Read = true;	
 						}
@@ -874,7 +876,7 @@ class GeCoS_IO extends IPSModule
 				}
 			}
 		 }
-		 
+		 /*
 		 If ($Bit17Read) {
 			 IPS_Sleep(100);
 			 $this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"interrupt", "DeviceBus" => 4)));
@@ -899,6 +901,7 @@ class GeCoS_IO extends IPSModule
 			 IPS_Sleep(100);
 			 $this->CheckSerial();
 		 } 
+		 */
 	}
  
 	// Aktualisierung der genutzten Pins und der Notifikation
