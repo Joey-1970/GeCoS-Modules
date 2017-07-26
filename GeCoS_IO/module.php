f1db9b28ae485806f1b6bab03eedde85e625aa00
Joey-1970 <paeper@horburg.de>
2017-07-26 21:09:36 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -1116,7 +1116,7 @@ class GeCoS_IO extends IPSModule
 		            	If ($response[4] >= 0) {
 		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
 					$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_data", "InstanceID" => $this->InstanceArraySearch("Handle", $response[2]), "Register" => $response[3], "Value" => $response[4])));
-					$this->SendDebug("Befehl 61", "erfolgreich", 0);
+					//$this->SendDebug("Befehl 61", "erfolgreich", 0);
 		            	}
 		            	else {
 		            		IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));
