96b60b8a4498592b6bedaeaf28815707762ad52e
Joey-1970 <paeper@horburg.de>
2017-07-29 23:56:16 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -302,9 +302,9 @@ class GeCoS_IO extends IPSModule
 					// I²C Bus 1 für RTC, Serielle Schnittstelle,
 					//Notify Pin 17 + 27 + 15= Bitmask 134381568
 					//$this->ClientSocket(pack("L*", 19, $this->GetBuffer("Handle"), (pow(2, 15) + pow(2, 17) + pow(2, 27)), 0), 16);
-					$this->CommandClientSocket(pack("L*", 19, $this->GetBuffer("Handle"), 134381568), 16);
-					$this->ClientSocket(pack("L*", 19, $this->GetBuffer("Handle"), 134381568), 16);
 					$this->SetBuffer("NotifyCounter", 0);
+					$this->CommandClientSocket(pack("L*", 19, $this->GetBuffer("Handle"), 134381568), 16);
+					$this->ClientSocket(pack("L*", 19, $this->GetBuffer("Handle"), 134381568), 16);	
 				}
 				
 				$this->SetStatus(102);
