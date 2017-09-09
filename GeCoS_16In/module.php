48ace503b346461b97b7e8e2372b9ca7729791ec
Joey-1970 <paeper@horburg.de>
2017-09-09 12:35:21 +0200

    Update module.php

--- GeCoS_16In/module.php
+++ GeCoS_16In/module.php
@@ -68,11 +68,13 @@
 				//ReceiveData-Filter setzen
 				$Filter = '((.*"Function":"get_used_i2c".*|.*"InstanceID":'.$this->InstanceID.'.*)|(.*"Function":"status".*|.*"Function":"interrupt".*))';
 				$this->SetReceiveDataFilter($Filter);
-				$this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));		
-				// Setup
-				$this->Setup();
-				$this->GetInput();
-				$this->SetTimerInterval("GetInput", 15 * 1000);
+				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));		
+				If ($Result == true) {
+					// Setup
+					$this->Setup();
+					$this->GetInput();
+					$this->SetTimerInterval("GetInput", 15 * 1000);
+				}
 			}
 			else {
 				$this->SetStatus(104);
