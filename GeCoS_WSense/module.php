75a6b3f11d015632a9eaa53ed1ace9489ef9296d
Joey-1970 <paeper@horburg.de>
2018-03-22 20:05:38 +0100

    Update module.php

--- GeCoS_WSense/module.php
+++ GeCoS_WSense/module.php
@@ -43,20 +43,13 @@
 		//Status-Variablen anlegen
 
 		
-		$this->SetBuffer("ErrorCounter", 0);
 		
 		If ((IPS_GetKernelRunlevel() == 10103) AND ($this->HasActiveParent() == true)) {			
 			If ($this->ReadPropertyBoolean("Open") == true) {	
 				//ReceiveData-Filter setzen
-				$Filter = '((.*"Function":"get_used_i2c".*|.*"InstanceID":'.$this->InstanceID.'.*)|(.*"Function":"status".*|.*"Function":"interrupt".*))';
-				$this->SetReceiveDataFilter($Filter);
-				$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "set_used_i2c", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "InstanceID" => $this->InstanceID)));		
-				If ($Result == true) {
-					// Setup
-					$this->Setup();
-					$this->GetInput();
-					$this->SetTimerInterval("GetInput", 15 * 1000);
-				}
+				
+				
+				$this->SetStatus(102);
 			}
 			else {
 				$this->SetStatus(104);
@@ -72,82 +65,14 @@
 	{
 	    	// Empfangene Daten vom Gateway/Splitter
 	    	$data = json_decode($JSONString);
-	 	switch ($data->Function) {
-			case "get_used_i2c":
-			   	If ($this->ReadPropertyBoolean("Open") == true) {
-					$this->ApplyChanges();
-				}
-				break;
-			case "status":
-			   	If ($data->InstanceID == $this->InstanceID) {
-				   	If ($this->ReadPropertyBoolean("Open") == true) {				
-						$this->SetStatus($data->Status);
-					}
-					else {
-						$this->SetStatus(104);
-					}	
-			   	}
-			   	break;
-			case "interrupt":
-				If ($this->ReadPropertyBoolean("Open") == true) {
-					If ($this->ReadPropertyInteger("DeviceBus") == $data->DeviceBus) {
-						$this->GetInput();
-					}
-				}
-				break;	
-			case "interrupt_with_result":
-				If (($this->ReadPropertyBoolean("Open") == true) AND ($data->InstanceID == $this->InstanceID)) {
-					$this->SendDebug("interrupt_with_result", "Ausfuehrung", 0);
-					for ($i = 0; $i <= 15; $i++) {
-						$Bitvalue = boolval(intval($data->Value) & pow(2, $i));					
-						If (GetValueBoolean($this->GetIDForIdent("Input_X".$i)) <> $Bitvalue) {
-							SetValueBoolean($this->GetIDForIdent("Input_X".$i), $Bitvalue);
-						}
-					}
-				}
-				break;		
+	 	
+	
 	 	}
  	}
 	    
 	// Beginn der Funktionen
-	public function GetInput()
-	{
-		$this->SendDebug("GetInput", "Ausfuehrung", 0);
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			$Result= $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_PCA9655E_Read", "InstanceID" => $this->InstanceID, "Register" => 0)));
-			if (($Result === NULL) OR ($Result < 0) OR ($Result > 65535)) {// Falls der Splitter einen Fehler hat und 'nichts' zurückgibt.
-				$this->SetBuffer("ErrorCounter", ($this->GetBuffer("ErrorCounter") + 1));
-				$this->SendDebug("GetInput", "Keine gueltige Antwort:".$Result, 0);
-				If ($this->GetBuffer("ErrorCounter") <= 3) {
-					$this->GetInput();
-				}
-			}
-			else {
-				$this->SendDebug("GetInput", "Ergebnis: ".$Result, 0);
-				for ($i = 0; $i <= 15; $i++) {
-					$Bitvalue = boolval($Result & pow(2, $i));					
-					If (GetValueBoolean($this->GetIDForIdent("Input_X".$i)) <> $Bitvalue) {
-						SetValueBoolean($this->GetIDForIdent("Input_X".$i), $Bitvalue);
-					}
-				}
-				$this->SetBuffer("ErrorCounter", 0);
-			}
-		}
-	}
-	private function Setup()
-	{
-		$this->SendDebug("Setup", "Ausfuehrung", 0);
-		If ($this->ReadPropertyBoolean("Open") == true) {
-			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "i2c_PCA9655E_Write", "InstanceID" => $this->InstanceID, "Register" => 6, "Value" => 65535 )));
-			If ($Result) {
-				$this->SendDebug("Setup", "erfolgreich", 0);
-			}
-			else {
-				$this->SendDebug("Setup", "nicht erfolgreich!", 0);
-				IPS_LogMessage("GeCoS_16In", "Setup: nicht erfolgreich!");
-			}
-		}
-	}    
+
+
 	    
 	private function HasActiveParent()
     	{
