b9ea6ae6f2a00efbb03583401cd91ec1ad63bfdc
Joey-1970 <paeper@horburg.de>
2018-03-26 14:16:13 +0200

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -577,6 +577,22 @@ class GeCoS_IO extends IPSModule
 								   pack("L*", 62, $I2CInstanceArray[$data->InstanceID]["Handle"], $data->Register + 11, 4, $data->Value_12), 192);
 				}
 		   		break;
+			case "i2c_MCP23017_write": 
+				// I2CWI h r bvs - smb Write I2C Block Data
+				If ($I2CInstanceArray[$data->InstanceID]["Handle"] >= 0) {
+					$this->SetMUX($I2CInstanceArray[$data->InstanceID]["DeviceBus"]);
+					$ParameterArray = array();
+					$ParameterArray = unserialize($data->Parameter);
+					$Result = $this->CommandClientSocket(pack("LLLLC*", 68, $this->GetI2C_DeviceHandle(intval($data->DeviceIdent)), $data->Register, count($ParameterArray), 
+										  ...$ParameterArray), 16);
+				}
+				break;
+			case "i2c_MCP23017_read":
+				If ($I2CInstanceArray[$data->InstanceID]["Handle"] >= 0) {
+					$this->SetMUX($I2CInstanceArray[$data->InstanceID]["DeviceBus"]);
+					$Result = $this->CommandClientSocket(pack("L*", 67, $this->GetI2C_DeviceHandle(intval($data->DeviceIdent)), $data->Register, 4, $data->Count), 16 + ($data->Count));
+				}
+				break;  
 			// Serielle Kommunikation
 			case "serial_write":
 				$Message = utf8_decode($data->Message);
