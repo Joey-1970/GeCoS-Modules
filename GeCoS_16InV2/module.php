28223580fdb77f15b1d6d2896ff473a0c2f8fc21
Joey-1970 <paeper@horburg.de>
2018-03-26 14:38:01 +0200

    Update module.php

--- GeCoS_16InV2/module.php
+++ GeCoS_16InV2/module.php
@@ -24,6 +24,7 @@
 		$arrayStatus[] = array("code" => 104, "icon" => "inactive", "caption" => "Instanz ist inaktiv");
 		$arrayStatus[] = array("code" => 200, "icon" => "error", "caption" => "Instanz ist fehlerhaft");
 		$arrayStatus[] = array("code" => 201, "icon" => "error", "caption" => "Device konnte nicht gefunden werden");
+		$arrayStatus[] = array("code" => 202, "icon" => "error", "caption" => "I²C-Kommunikationfehler!");
 		
 		$arrayElements = array(); 
 		$arrayElements[] = array("name" => "Open", "type" => "CheckBox",  "caption" => "Aktiv"); 
