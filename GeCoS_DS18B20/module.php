3706b473108a3ef521e277d5eea03bce7b772d20
Joey-1970 <paeper@horburg.de>
2018-07-04 16:16:01 +0200

    Update module.php

--- GeCoS_DS18B20/module.php
+++ GeCoS_DS18B20/module.php
@@ -89,6 +89,9 @@
         {
             	// Diese Zeile nicht löschen
             	parent::ApplyChanges();
+		
+		// Summary setzen
+		$this->SetSummary("SC: ".$this->ReadPropertyString("DeviceAddress"));
             	
 		$OWDeviceArray = Array();
 		$this->SetBuffer("OWDeviceArray", serialize($OWDeviceArray));
