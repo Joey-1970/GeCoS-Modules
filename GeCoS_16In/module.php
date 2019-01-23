7102f60e31e93741b5ca3324f784c551bb365780
Joey-1970 <paeper@horburg.de>
2019-01-23 20:13:57 +0100

    Update module.php

--- GeCoS_16In/module.php
+++ GeCoS_16In/module.php
@@ -54,6 +54,9 @@
 		$arrayOptions = array();
 		$arrayOptions[] = array("label" => "GeCoS I²C-Bus 0", "value" => 4);
 		$arrayOptions[] = array("label" => "GeCoS I²C-Bus 1", "value" => 5);
+		If ($this->GetBoardVersion() == 1) {
+			$arrayOptions[] = array("label" => "GeCoS I²C-Bus 2", "value" => 6);
+		}
 		
 		$arrayElements[] = array("type" => "Select", "name" => "DeviceBus", "caption" => "GeCoS I²C-Bus", "options" => $arrayOptions );
 		$arrayElements[] = array("type" => "Label", "label" => "_____________________________________________________________________________________________________");
@@ -425,6 +428,12 @@
 			}
 		}
 	}    
+	
+	private function GetBoardVersion()
+	{
+		$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "getBoardVersion" )));	
+	return $Result;
+	}
 	    
 	private function HasActiveParent()
     	{
