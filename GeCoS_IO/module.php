a1ddd5b581d7440336d7dbfbba54b672315ce0eb
Joey-1970 <paeper@horburg.de>
2019-01-28 19:44:17 +0100

    Update module.php

--- GeCoS_IO/module.php
+++ GeCoS_IO/module.php
@@ -1280,11 +1280,12 @@ class GeCoS_IO extends IPSModule
 					$ByteMessage = substr($Message, -($response[4]));
 					$ByteResponse = unpack("C*", $ByteMessage);
 					$ByteArray = serialize($ByteResponse);
-					$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_byte_block", "InstanceID" => $this->InstanceArraySearch("Handle", $response[2]), "Register" => $response[3], "Count" => $response[4], "ByteArray" => $ByteArray)));
-					$Result = $ByteArray; 
+					//$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_byte_block", "InstanceID" => $this->InstanceArraySearch("Handle", $response[2]), "Register" => $response[3], "Count" => $response[4], "ByteArray" => $ByteArray)));
+					$Result = serialize($ByteResponse);
 				}
 		            	else {
-           				IPS_LogMessage("GeCoS_IO I2C Read Bytes on Handle","Handle: ".$response[2]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));
+           				$Result = false;
+					IPS_LogMessage("GeCoS_IO I2C Read Bytes on Handle","Handle: ".$response[2]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));
            			}
 				break;
 			case "57":
@@ -1308,11 +1309,12 @@ class GeCoS_IO extends IPSModule
 		            	break;
 			case "60":
 		            	If ($response[4] >= 0) {
-		            		//IPS_LogMessage("GeCoS_IO I2C Read Byte","Handle: ".$response[2]." Register: ".$response[3]." Value: ".$response[4]." DeviceSign: ".$this->GetI2C_HandleDevice($response[2]));
+		            		$Result = true;
 					//$this->SendDataToChildren(json_encode(Array("DataID" => "{573FFA75-2A0C-48AC-BF45-FCB01D6BF910}", "Function"=>"set_i2c_data", "InstanceID" => $this->InstanceArraySearch("Handle", $response[2]), "Register" => $response[3], "Value" => $response[4])));
 		            	}
 		            	else {
-		            		IPS_LogMessage("GeCoS_IO I2C Write Byte on Handle","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));	
+		            		$Result = true;
+					IPS_LogMessage("GeCoS_IO I2C Write Byte on Handle","Handle: ".$response[2]." Register: ".$response[3]." Fehlermeldung: ".$this->GetErrorText(abs($response[4])));	
 		            	}
 		            	break;
 			case "61":
