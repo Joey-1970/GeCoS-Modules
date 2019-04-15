b9999c664ed33d1f8f07f1ffe8fdc2f8b7538616
Joey-1970 <paeper@horburg.de>
2019-04-15 20:27:54 +0200

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -526,7 +526,7 @@
 	return $Result;
 	}
 	    
-	private function HasActiveParent()
+	protected function HasActiveParent()
     	{
 		$Instance = @IPS_GetInstance($this->InstanceID);
 		if ($Instance['ConnectionID'] > 0)
