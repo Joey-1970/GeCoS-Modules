22f2b72e0a969b6b533a3e1a18682189e2e1b771
Joey-1970 <paeper@horburg.de>
2019-06-05 20:00:21 +0200

    Update module.php

--- GeCoS_16Out/module.php
+++ GeCoS_16Out/module.php
@@ -161,7 +161,7 @@
 			}
 			
 			$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "SOM", "DeviceAddress" => $this->ReadPropertyInteger("DeviceAddress"), "DeviceBus" => $this->ReadPropertyInteger("DeviceBus"), "Value" => $Bitmask )));
-			
+			$this->SendDebug("SetOutputPin", "Result: ".$Result, 0);
 			If ($Result == true) {
 				$this->SetStatus(102);
 				for ($i = 0; $i <= 15; $i++) {
