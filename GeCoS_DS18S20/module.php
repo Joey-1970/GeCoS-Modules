8e0dcbd4c2bfec0ef19f42067e0cc0d51481cfe7
Joey-1970 <paeper@horburg.de>
2019-06-24 19:10:12 +0200

    Update module.php

--- GeCoS_DS18S20/module.php
+++ GeCoS_DS18S20/module.php
@@ -38,7 +38,7 @@
 		$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{47113C57-29FE-4A60-9D0E-840022883B89}", "Function" => "get_OWDevices", "FamilyCode" => "10", "InstanceID" => $this->InstanceID)));
 		$OWDeviceArray = Array();
 		$OWDeviceArray = unserialize($Result);
-		$OWDeviceArray = unserialize($this->GetBuffer("OWDeviceArray"));
+		//$OWDeviceArray = unserialize($this->GetBuffer("OWDeviceArray"));
 		If ($this->ReadPropertyString("DeviceAddress") == "Sensorauswahl") {
 			$arrayValues = Array();
 			$arrayValues[] = array("name" => "DeviceAddress", "value" => "Sensorauswahl");
